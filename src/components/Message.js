d23da6b723583de648ff6b0dbc0a2f08b44fe604
John Maddock <jmmaddock@gmail.com>
2018-12-04 11:34:18 -0700

    working on it

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,23 +1,34 @@
 import React from "react"
 
-const Message = ({ messages }) => {
-  console.log(messages)
+const Message = ({ messages, toggleSelect, toggleStar }) => {
+  const readClass = messages.read ? 'read' : 'unread'
+  const selectedClass = messages.selected ? 'selected' : ""
+  const starClass = messages.starred ? 'fa-star' : 'fa-star-o'
+
+  const labels = messages.labels.map((label, i) => (
+    <span key={i} className="label label-warning">{label}</span>
+  ))
+
+  const starMessage = (e) => {
+    e.stopPropagtion()
+    toggleStar(messages)
+  }
+
   return (
-    <div className={`row message read selected textLeft`}>
+    <div className={`row message ${readClass} ${selectedClass}`} onClick={() => toggleSelect(messages)}>
       <div className="col-xs-1">
         <div className="row">
           <div className="col-xs-2">
-            <input type="checkbox" checked="checked" />
+            <input type="checkbox" checked={!!messages.selected} readOnly={true} />
           </div>
-          <div className="col-xs-2">
-            <i className={`star fa starred`} />
+          <div className="col-xs-2" onClick={starMessage}>
+            <i className={`star fa ${starClass}`}></i>
           </div>
         </div>
       </div>
       <div className="col-xs-11">
-        {/* Labels */}
-
-        <a href="#!">{messages.subject}</a>
+        {labels}
+        {messages.subject}
       </div>
     </div>
   )
